66d36a6307b113c74b63718fcf7872f6058eb45d
DavidZelaya12 <davidzelaya06.isj@gmail.com>
2025-02-26 15:40:38 -0600

    Implementacion de validator.h

---
+++ lib/Validator.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+class Validator
+{
+public:
+    Validator()
+    {
+        std::cout << "Validator created" << std::endl;
+    }
+
+    bool Is_validOperator(std::string hola)
+    {
+        char token = hola[0];
+        return (token == '+') ?: ((token == '-') ? true : ((token == '*') ? true : ((token == '/') ? true : (token == '^') ? true
+                                                                                                                           : ((token == '%') ? true : false))));
+    }
+};
