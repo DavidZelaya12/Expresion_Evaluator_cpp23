fa3bccb5229bd16c50f3814ff6d956826c50d163
DavidZelaya12 <davidzelaya06.isj@gmail.com>
2025-03-20 10:21:06 -0600

    Agregado de funciones de apoyo

--- lib/Validator.h
+++ lib/Validator.h
@@ -1,3 +1,6 @@
+#ifndef VALIDATOR_H
+#define VALIDATOR_H
+#include <algorithm>
 #include <iostream>
 #include <string>
 
@@ -5,7 +8,7 @@ class Validator {
 public:
   Validator() { std::cout << "Validator creado" << std::endl; }
 
-  bool Is_validOperator(std::string hola) {
+  bool ValidarOperador(std::string hola) {
     char token = hola[0];
     return (token == '+')
                ?: ((token == '-')
@@ -17,4 +20,26 @@ public:
                                      ? true
                                      : ((token == '%') ? true : false))));
   }
+
+  int Precedencia(char token) {
+    return (token == '^') ? 3
+                          : ((token == '*' || token == '/')
+                                 ? 2
+                                 : ((token == '+' || token == '-') ? 1 : -1));
+  }
+
+  bool ValidarNumero(const std::string &str) {
+    return !str.empty() && std::all_of(str.begin(), str.end(), ::isdigit);
+  }
+
+  int MayorPrecedencia(char token1, char token2) {
+    // -1 == <
+    //  1 == >
+    //  0 == ==
+    return (Precedencia(token1) > Precedencia(token2))
+               ? 1
+               : ((Precedencia(token1) < Precedencia(token2)) ? -1 : 0);
+  }
 };
+
+#endif
