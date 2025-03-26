74b71a0a6422cf04019798bc9a1e37647900145a
DavidZelaya12 <davidzelaya06.isj@gmail.com>
2025-03-26 08:04:09 -0600

    Implementacion de main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include "ExpresionEvaluator.h"
+#include "Parser.h"
+#include <Variables.h>
 #include <iostream>
 
-int main() {
-  ExpresionEvaluator Evaluator;
+int main(int argc, char *argv[]) {
+  ExpresionEvaluator evaluator;
+  Parser parser;
+  std::string expresion = argv[1];
 
-    return 0;
+  std::cout << "|----Evaluador de expresiones----|" << std::endl;
+  auto result = parser.Validar(expresion);
+  std::string ValidExpresion = "";
+
+  if (result.has_value()) {
+    std::cout << "Sintaxis correcta" << std::endl;
+    parser.setExpresion(ValidExpresion);
+  } else {
+    std::cout << result.error() << std::endl;
+    return 1;
+  }
+
+  std::cout << "--Evaluacion--" << std::endl;
+  evaluator.setExpresion(ValidExpresion);
+  evaluator.EvaluarExpresion();
+  std::cout << "|-----------------------------|" << std::endl;
+  return 0;
 }
