277a386d127c8cee4d090bd60e33db4eeea137ee
DavidZelaya12 <davidzelaya06.isj@gmail.com>
2025-03-26 08:03:17 -0600

    Modificado a ruta estatica para uso de variables

--- lib/Variables.h
+++ lib/Variables.h
@@ -4,6 +4,8 @@ un map<string, double> para poder ser utilizado en el programa principal
 y cargar cualquier variable que se necesite a futuros en el evaluador de
 expresiones
 */
+#ifndef VARIABLES_H
+#define VARIABLES_H
 #include <fstream>
 #include <iostream>
 #include <map>
@@ -13,24 +15,29 @@ class Variables {
 public:
   std::map<std::string, double> variableMap;
 
-  Variables() {
-    std::cout << "Variables creado" << std::endl;
-    InitVariables();
-  }
+  Variables() { InitVariables(); }
 
   void InitVariables() {
-    std::ifstream archivo("variables.txt");
+    std::ifstream archivo("C:\\Users\\HP\\Desktop\\Github\\Expresion_Evaluator_"
+                          "cpp23\\variables.txt");
     std::string linea;
+
+    if (!archivo.is_open()) {
+      std::cout << "No se pudo abrir el archivo" << std::endl;
+      return;
+    }
+
     while (std::getline(archivo, linea)) {
 
       if (linea.empty()) {
         continue;
       }
-
       std::string variable = linea.substr(0, linea.find("="));
       std::string valor = linea.substr(linea.find("=") + 1);
       variableMap[variable] = std::stod(valor);
     }
+    std::cout << "Variables cargadas" << std::endl;
+    MostrarVariables();
   }
 
   void MostrarVariables() {
@@ -49,3 +56,5 @@ public:
 
   double ObtenerValor(std::string variable) { return variableMap[variable]; }
 };
+
+#endif
